435eed4cc4ca82f48f5fd8912ea8e0efc5f088a7
Lisa McBride <lisaleemcb@gmail.com>
2025-01-21 15:01:08 +0100

    rm models.py

--- src/alfred/__init__.py
+++ src/alfred/__init__.py
@@ -1,5 +1,7 @@
 import sys
 
+from . import models
+
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
@@ -14,5 +16,3 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
-
-from . import models
