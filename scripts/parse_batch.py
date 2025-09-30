e8936ba36c5cdef72df1058c3ed65e13451592d2
Lisa McBride <lisaleemcb@gmail.com>
2025-09-30 16:14:57 +0200

    added script for saving results of batch fit

---
+++ scripts/parse_batch.py
@@ -0,0 +1,120 @@
+import os
+from random import sample
+import re
+import time
+import copy as cp
+import argparse
+import toml
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+import pandas as pd
+import zeus
+
+from scipy.interpolate import PchipInterpolator, CubicSpline
+
+import alfred.utils as utils
+import alfred.emulator as emulator
+import alfred.KSZ as KSZ
+import alfred.peefit as peefit
+import alfred.surveys as surveys
+
+import joblib
+import keras
+import tensorflow as tf
+tf.config.set_visible_devices([], 'GPU')
+
+import warnings
+from types import SimpleNamespace
+from scipy.interpolate import RegularGridInterpolator, RectBivariateSpline
+from catwoman.shelter import Cat
+
+import alfred.surveys as surveys
+import alfred.emulator as emulator
+import alfred.utils as utils
+from alfred.parameters import *
+from alfred.astrofit import *
+
+def parse_batch(directory, ext):
+    sampled_sims = []
+    truths = []
+    medians = []
+    low68 = []
+    high68 = []
+    low95 = []
+    high95 = []
+
+    pattern = re.compile(r'(\d{5})')
+    for i in range(10):
+        print(f"On {i}")
+
+        # directory = f"{base_dir}/inference/biases_randomseed5/samples_{i}"
+        for filename in os.listdir(directory):
+            filepath = os.path.join(directory, filename)
+            # print(filepath)
+
+            match = pattern.search(filename)
+            if match:
+                sn = match[0]
+                # print(f"On sim {sn}")
+                if sn in sampled_sims:
+                    print(f'resampled')
+                    continue
+                if sn in ['18861', '19048', '17587', '17734']:
+                    continue
+                if not os.path.exists(f"{filepath}/saved_chains.h5"):
+                    continue
+
+                s, l = utils.load_samples(filepath, verbose=False)
+                samples = make_tauchains(s, truths=df.loc[sn].to_dict(), dropA=True)
+                truth = [*df.loc[sn].to_numpy(), *whatthetau(df.loc[sn].to_numpy())]
+
+                ci = 68
+                lower_q = (100 - ci) / 2
+                upper_q = 100 - lower_q
+                low, high = np.percentile(samples, [lower_q, upper_q], axis=0)
+                low68.append(low)
+                high68.append(high)
+
+                ci = 95
+                lower_q = (100 - ci) / 2
+                upper_q = 100 - lower_q
+                low, high = np.percentile(samples, [lower_q, upper_q], axis=0)
+                low95.append(low)
+                high95.append(high)
+
+
+                sampled_sims.append(sn)
+                truths.append(truth)
+                medians.append(np.median(s, axis=0))
+
+    sampled_sims = np.asarray(sampled_sims)
+    truths = np.asarray(truths)
+    medians = np.asarray(medians)
+    low68 = np.asarray(low68)
+    high68 = np.asarray(high68)
+    low95 = np.asarray(low95)
+    high95 = np.asarray(high95)
+
+    np.save(f'sampledsims_{ext}', sampled_sims)
+    np.save(f'truths_{ext}', truths)
+    np.save(f'medians_{ext}', medians)
+    np.save(f"low68_{ext}", low68)
+    np.save(f"high68_{ext}", high68)
+    np.save(f"low95_{ext}", low95)
+    np.save(f"high95_{ext}", high95)
+
+    return
+
+def main()
+    directory = f"{base_dir}/inference/biases/samples_{i}"
+    print(f"Now running through {directory}...")
+    parse_batch(directory, 'v_1')
+
+    directory = f"{base_dir}/inference/biase_rerun_lowernoise/samples_{i}"
+    print(f"Now running through {directory}...")
+    parse_batch(directory, 'v_2')
+
+if __name__ == "__main__":
+    main()
