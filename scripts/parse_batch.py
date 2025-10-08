235882afe09247a26122cb42c257a98a6099b02c
Lisa McBride <lisaleemcb@gmail.com>
2025-10-08 11:00:39 +0200

    fixed indexing error

--- scripts/parse_batch.py
+++ scripts/parse_batch.py
@@ -81,7 +81,7 @@ def parse_batch(dir, ext):
                 )
                 ravg.append(np.mean(np.where(samples < truth[None, 2:], 1, 0), axis=0))
 
-                samples_random = draws(ndraws=samples.shape[0])[2:]
+                samples_random = draws(ndraws=samples.shape[0])[:, 2:]
                 ravg_random.append(
                     np.mean(np.where(samples_random < truth[None, 2:], 1, 0), axis=0)
                 )
--- src/alfred/parameters.py
+++ src/alfred/parameters.py
@@ -9,65 +9,72 @@ import matplotlib.pyplot as plt
 # home_dir = '/home/emc-brid'
 # base_dir = '/data/cluster/emc-brid/Datasets/LoReLi'
 
-home_dir = '/Users/emcbride'
-base_dir = '/Users/emcbride/Datasets/LoReLi'
+home_dir = "/Users/emcbride"
+base_dir = "/Users/emcbride/Datasets/LoReLi"
 
 ##########################
 ####  Helpful things  ####
 ##########################
-box_size= 296.0 # Mpc
+box_size = 296.0  # Mpc
 k_res = ((2 * np.pi) / box_size, (2 * np.pi * 256) / box_size / 2)
 k_bins = np.geomspace(k_res[0], k_res[1], 26)
 k_center = []
-for i in range(k_bins.size-1):
-    k_center.append((k_bins[i] + k_bins[i+1]) / 2)
+for i in range(k_bins.size - 1):
+    k_center.append((k_bins[i] + k_bins[i + 1]) / 2)
 
 k_center = np.asarray(k_center)
 
 norm_z = matplotlib.colors.Normalize(vmin=3.0, vmax=22)
-cmap_z = plt.get_cmap('viridis')
+cmap_z = plt.get_cmap("viridis")
 
 norm_xe = matplotlib.colors.LogNorm(vmin=1.7e-4, vmax=1.16)
-cmap_xe = plt.get_cmap('plasma')
+cmap_xe = plt.get_cmap("plasma")
 
-colors = ['green', 'blue', 'plum', 'goldenrod', 'midnightblue', 'deeppink', 'tomato']
+colors = ["green", "blue", "plum", "goldenrod", "midnightblue", "deeppink", "tomato"]
 
-astro_labels = ['Xray_Lfunc', 'hard_Xray_fraction',
-                'gasconversion_timescale', 'log10_Mmin', 'ion_escapefrac_post']
+astro_labels = [
+    "Xray_Lfunc",
+    "hard_Xray_fraction",
+    "gasconversion_timescale",
+    "log10_Mmin",
+    "ion_escapefrac_post",
+]
 
-astro_pnames = ['L_X', 'f_X', 'tau', 'Mmin', 'f_esc']
+astro_pnames = ["L_X", "f_X", "tau", "Mmin", "f_esc"]
 astro_pnames_formatted = [
-    r'$L_X$',
-    r'$f_X$',
-    r'$\tau_{\mathrm{SF}}$',
-    r'$M_{\mathrm{min}}$',
-    r'$f_{\mathrm{esc}}$',
-    r'$\tau_{\mathrm{CMB}}$',
-    r'$A_{\mathrm{hkSZ}}$'
+    r"$L_X$",
+    r"$r_{\mathrm{H/S}}$",
+    r"$\tau_{\mathrm{SF}}$",
+    r"$M_{\mathrm{min}}$",
+    r"$f_{\mathrm{esc}}$",
+    r"$\tau_{\mathrm{CMB}}$",
+    r"$A_{\mathrm{hkSZ}}$",
 ]
 
 astro_labels_formatted = [
- 'Xray \n Lfunc',
- 'hard \n Xray \n fraction'
- 'gas \n conversion \n timescale',
- 'log10_Mmin',
- 'ion \n escapefrac \n post']
+    "Xray \n Lfunc",
+    "hard \n Xray \n fractiongas \n conversion \n timescale",
+    "log10_Mmin",
+    "ion \n escapefrac \n post",
+]
 
 ##########################
 ####  Fitting params  ####
 ##########################
 
 kmin = k_res[0]
-kmax = 1.5 # To avoid the splitting between the LoReLi spectra and Gorce model at high k
+kmax = (
+    1.5  # To avoid the splitting between the LoReLi spectra and Gorce model at high k
+)
 
-xemin = .01
-xemax = .97
+xemin = 0.01
+xemax = 0.97
 
 
 ##########################
 #### Cosmo parameters ####
 ##########################
-h = 0.678 #* units.km / units.s / units.Mpc
+h = 0.678  # * units.km / units.s / units.Mpc
 Om_0 = 0.308
 OL_0 = 0.692
 Ob_0 = 0.0484
@@ -76,18 +83,14 @@ och2 = (Om_0 - Ob_0) * h**2
 s8 = 0.815
 T_cmb = 2.7255
 
-cosmology_LoReLi=cosmology.FlatLambdaCDM(H0=h*100,Tcmb0=T_cmb,Ob0=Ob_0,Om0=Om_0)
-cosmoparams_LoReLi = {'H0': h * 100,
-                    'Om_0': Om_0,
-                    'OL_0': OL_0,
-                    'Ob_0': Ob_0,
-                    's8': s8}
+cosmology_LoReLi = cosmology.FlatLambdaCDM(H0=h * 100, Tcmb0=T_cmb, Ob0=Ob_0, Om0=Om_0)
+cosmoparams_LoReLi = {"H0": h * 100, "Om_0": Om_0, "OL_0": OL_0, "Ob_0": Ob_0, "s8": s8}
 
 Yp = 0.2453
-Xp = 1-Yp
-mh = constants.m_n.value #kg
-rhoc = cosmology_LoReLi.critical_density0.si.value #kg m-3
-nh = Xp*Ob_0*rhoc/mh  # m-3
+Xp = 1 - Yp
+mh = constants.m_n.value  # kg
+rhoc = cosmology_LoReLi.critical_density0.si.value  # kg m-3
+nh = Xp * Ob_0 * rhoc / mh  # m-3
 xe_recomb = 1.7e-4
 
 # T_CMB=2.7260 #K
@@ -96,8 +99,8 @@ xe_recomb = 1.7e-4
 ###################
 #### Constants ####
 ###################
-s_T = constants.sigma_T.value    # sigma_thomson in SI units [m^2]
-c = constants.c.value   # speed of light in SI units [m.s-1]
+s_T = constants.sigma_T.value  # sigma_thomson in SI units [m^2]
+c = constants.c.value  # speed of light in SI units [m.s-1]
 Mpcm = (1.0 * units.Mpc).to(units.m).value  # one Mpc in [m]
 Mpckm = Mpcm / 1e3
 
@@ -106,24 +109,24 @@ Mpckm = Mpcm / 1e3
 #######################################
 
 # parameters for reionisation history
-asym = True #asymmetric or tanh model for xe(z)
+asym = True  # asymmetric or tanh model for xe(z)
 zend = 5.5
-zre = 7.
-z_early = 20.
+zre = 7.0
+z_early = 20.0
 
 # reionisation of Helium
 HeliumI = True
 HeliumII = False
-fH = 1.
+fH = 1.0
 if HeliumI:
-	not4 = 3.9715 #eta
-	fHe = Yp/(not4*(1-Yp))
-	fH=1+2*fHe
+    not4 = 3.9715  # eta
+    fHe = Yp / (not4 * (1 - Yp))
+    fH = 1 + 2 * fHe
 helium_fullreion_redshift = 3.5
 helium_fullreion_start = 5.0
 helium_fullreion_deltaredshift = 0.5
 
-astro_fiducial = {'fH': fH}
+astro_fiducial = {"fH": fH}
 
 ##########################
 #### Model parameters ####
@@ -132,21 +135,25 @@ astro_fiducial = {'fH': fH}
 alpha0 = 3.7
 kappa = 0.10
 
-modelparams_Gorce2022 = {'alpha0': 3.93,
-                        'kappa': 0.084,
-                        'a_xe': -1.0 / 5.0,
-                        'k_xe': 1.0,
-                        'k_f': 9.4,
-                        'g': .5,
-				        'B': 0.0}
+modelparams_Gorce2022 = {
+    "alpha0": 3.93,
+    "kappa": 0.084,
+    "a_xe": -1.0 / 5.0,
+    "k_xe": 1.0,
+    "k_f": 9.4,
+    "g": 0.5,
+    "B": 0.0,
+}
 
-KSZ_params = {'alpha0': 3.7,
-                'kappa': 0.1,
-                'a_xe': -0.2,
-                'k_xe': 1.0,
-                'k_f': 9.4,
-                'g': 0.5,
-				'B': 0.0}
+KSZ_params = {
+    "alpha0": 3.7,
+    "kappa": 0.1,
+    "a_xe": -0.2,
+    "k_xe": 1.0,
+    "k_f": 9.4,
+    "g": 0.5,
+    "B": 0.0,
+}
 
 
 #########################################
@@ -154,7 +161,7 @@ KSZ_params = {'alpha0': 3.7,
 #########################################
 ### linear ell range for kSZ C_ells
 
-ells_old = np.linspace(1,15000, 30)
+ells_old = np.linspace(1, 15000, 30)
 ells = np.arange(0, 15000, 500)
 ells[0] = 100
 
@@ -171,8 +178,7 @@ blobnames = [
     "hksz",
     "tt_cmb",
     "ee_cmb",
-    "te_cmb"
-    "tau",
+    "te_cmbtau",
 ]
 # priors
 z_max = 20.0
@@ -255,7 +261,7 @@ percentile2 = CL + (100 - CL) / 2
 smoothing = 1.0
 
 # plotting parameters
-ylabels = [ 'TT', 'EE', 'TE', 'pkSZ', 'hkSZ']
+ylabels = ["TT", "EE", "TE", "pkSZ", "hkSZ"]
 props = dict(boxstyle="round", facecolor="white", alpha=0.5)
 colorlist = [
     "#1f77b4",
